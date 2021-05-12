3bc829e214a85cbc39a6640bd05eca3d4180baf7
AshenOne-N <vincentchj@sina.com>
2021-05-12 14:32:29 +0800

    add logger

--- camWebServer/appCam.py
+++ camWebServer/appCam.py
@@ -24,7 +24,9 @@ def index():
 
 def gen(camera):
     """Video streaming generator function."""
+    current_app.logger.warning('this is from gen')
     while True:
+        
         frame = camera.get_frame()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
